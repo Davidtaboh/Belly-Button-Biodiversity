db8949297f1702dd05f7ececc0f8447ea0fc637d
davidtaboh <davidtaboh@yahoo.com>
2020-04-09 14:44:15 -0400

    Thursday afternoon add

---
+++ app.js
@@ -0,0 +1,26 @@
+// Read in file
+
+d3.json("Samples.json").then(function(data) {
+    console.log(data);
+  });
+
+// var dataset = data
+
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+
+// Use sample_values as the values for the bar chart.
+
+
+// Use otu_ids as the labels for the bar chart.
+
+
+// Use otu_labels as the hovertext for the chart.
+
+// Unsure of how to sort data
+
+// Data is "names", "metadata", "samples"
+
+
+
+
