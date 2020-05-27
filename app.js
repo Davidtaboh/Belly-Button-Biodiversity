5d1fca610102c8be0f3a626b0cbb1b84c75dafd6
davidtaboh <davidtaboh@yahoo.com>
2020-05-27 17:53:12 -0400

    Wednesday night add

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ for (let i = 0; i < data.names.length; i++) {
     var inputField = d3.select("#selDataset");
     inputField.on("change", function () {
       var user_pick_value = d3.event.target.value;
+      $('#sample-metadata').empty();
 
       const result = data.samples.filter(user => user.id == user_pick_value);
       const metadata = data.metadata.filter(user => user.id == user_pick_value);
@@ -35,8 +36,7 @@ for (let i = 0; i < data.names.length; i++) {
 
       var trace1 = {
         x: sample_values,
-        y: "OTU" + otu_ids,
-        // Y axis names not picking up correctly
+        y: otu_ids.map(otu => `OTU ${otu}`),
         orientation: "h",
         type: "bar"
       }
@@ -82,90 +82,4 @@ for (let i = 0; i < data.names.length; i++) {
 
   });
 
-  // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
-
-
-
-
-  // When userinput = the relevant Number
-  // return an array containing the correct data for that number
-
-  // var sampleValues = data.enterdatahere.map(v => v.sampleValues)
-
-  //       for (let i = 0; i < data.samples.length; i++) {
-
-//           let sample_values = data.samples[i].sample_values;
-//           let otu_ids = data.samples[i].otu_ids;
-
-//           console.log(sample_values)
-//           console.log(otu_ids)
-//           var trace1 = {
-//             x: sample_values,
-//             y: otu_ids,
-//             orientation: "h",
-//             type: "bar"
-//           }
-//            //var data = [trace1];
-
-//            console.log(trace1)
-
-//            // we need to sort and splice data
-
-//            var sortedData = data.sort((a, b) => b.sample_values - a.sample_values)
-//            console.log(sortedData)
-
-//           // Apply the group bar mode to the layout
-//           var layout = {
-//             title: "Greek gods search results",
-//             margin: {
-//               l: 100,
-//               r: 100,
-//               t: 100,
-//               b: 100
-//             }
-//           };
-
-//           console.log(layout)
-
-//           // // Render the plot to the div tag with id "plot"
-//            Plotly.newPlot("plot", [trace1], layout);
-//           // // break;
-
-
-
-//       }
-
-//     };
-
-//   };
-
-// });
-
-// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-// Use sample_values as the values for the bar chart.
-
-// Use otu_ids as the labels for the bar chart.
-
-
-// Use otu_labels as the hovertext for the chart.
-
-// Unsure of how to sort data
-
-// Data is "names", "metadata", "samples"
-
-
-// JQuery code i cant use
-
-// for (let i = 0; i < data.names.length; i++) {
-
-
-//   $('#selDataset').append(`<option value=${data.names[i]}>${data.names[i]} </option>`);
-// }
-
-// $('#selDataset').on('change', function () {
-
-//   let user_pick = $(this).val();
-//   // this isn't fltering data
-//   console.log(user_pick)
